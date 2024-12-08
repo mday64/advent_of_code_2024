a32db213e3fcfcd9762cbabd43b596c22004f8ea
Mark Day <mday@mac.com>
2024-12-08 11:59:11 -0700

    Day 1, part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,8 +1,15 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("../input.txt");
+
     let result1 = part1(input);
     println!("Part 1: {result1}");
     assert_eq!(result1, 2375403);
+    
+    let result2 = part2(input);
+    println!("Part 2: {result2}");
+    assert_eq!(result2, 23082277);
 }
 
 fn part1(input: &str) -> i32 {
@@ -18,6 +25,19 @@ fn part1(input: &str) -> i32 {
     left.iter().zip(right.iter()).map(|(l,r)| (l-r).abs()).sum()
 }
 
+fn part2(input: &str) -> u32 {
+    let mut left = Vec::<u32>::new();
+    let mut right = HashMap::<u32, u32>::new();
+    for line in input.lines() {
+        let mut words = line.split_whitespace();
+        left.push(words.next().expect("invalid input").parse().expect("invalid number"));
+        let r = words.next().expect("invalid input").parse().expect("invalid number");
+        *right.entry(r).or_insert(0) += 1;
+    }
+
+    left.iter().map(|v| v * right.get(v).unwrap_or(&0)).sum()
+}
+
 #[test]
 fn test_part1() {
     let input = "\
@@ -29,4 +49,17 @@ fn test_part1() {
 3   3
 ";
     assert_eq!(part1(input), 11);
+}
+
+#[test]
+fn test_part2() {
+    let input = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+    assert_eq!(part2(input), 31);
 }
