f1599f087bc75a5497d5c4681089aaf606a67b95
Mark Day <mday@mac.com>
2025-01-08 13:43:20 -0700

    Day 3, part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -9,6 +9,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Part 1: {result1}");
     assert_eq!(result1, 181345830);
 
+    let result2 = part2(input);
+    println!("Part 2: {result2}");
+    assert_eq!(result2, 98729041);
+
     Ok(())
 }
 
@@ -28,6 +32,16 @@ fn ignore_character(input: &str) -> IResult<&str, (u32, u32)> {
     Ok((remaining, (0, 0)))
 }
 
+fn parse_do(input: &str) -> IResult<&str, ()> {
+    let (remaining, _found) = tag("do()")(input)?;
+    Ok((remaining, ()))
+}
+
+fn parse_dont(input: &str) -> IResult<&str, ()> {
+    let (remaining, _found) = tag("don't()")(input)?;
+    Ok((remaining, ()))
+}
+
 fn part1(input: &str) -> u32 {
     let (_remaining, args) = many0(alt((mul_instruction, ignore_character)))(input).unwrap();
     args.iter()
@@ -35,8 +49,40 @@ fn part1(input: &str) -> u32 {
         .sum()
 }
 
+fn part2(input: &str) -> u32 {
+    let mut result = 0;
+    let mut remaining = input;
+    let mut enabled = true;
+
+    while !remaining.is_empty() {
+        if let Ok((r, (a,b))) = mul_instruction(remaining) {
+            eprintln!("mul(): {a} {b}{}", if enabled { "" } else { " (disabled)"});
+            remaining = r;
+            if enabled {
+                result += a * b;
+            }
+        } else if let Ok((r, _ignored)) = parse_do(remaining) {
+            remaining = r;
+            enabled = true;
+        } else if let Ok((r, _ignored)) = parse_dont(remaining) {
+            remaining = r;
+            enabled = false;
+        } else {
+            remaining = &remaining[1..];
+        }
+    }
+
+    result
+}
+
 #[test]
 fn test_part1() {
     let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     assert_eq!(part1(input), 161);
+}
+
+#[test]
+fn test_part2() {
+    let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    assert_eq!(part2(input), 48);
 }
