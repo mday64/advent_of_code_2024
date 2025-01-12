6a7bebcc9083aae063ccdef7e91aa8f78824fac8
Mark Day <mday@mac.com>
2025-01-12 15:45:32 -0700

    Day 6: starting package
    Based on _template, with the various things updated to be called "day06".

---
+++ day06/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn part1(_input: &str) -> u32 {
+    42
+}
+
+pub fn part2(_input: &str) -> u32 {
+    43
+}
+
+#[test]
+fn test_part1() {
+    let input = "Hello, World!";
+    assert_eq!(part1(input), 42);
+}
+
+#[test]
+fn test_part2() {
+    let input = "Hello, World!";
+    assert_eq!(part2(input), 43);
+}
