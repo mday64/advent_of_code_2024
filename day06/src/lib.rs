7a992b3d610da3d38ea4e7813ccc9e31293cdfcd
Mark Day <mday@mac.com>
2025-01-13 11:37:21 -0700

    Day 6, part 2 (SLOW)

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Direction {
     Up, Right, Down, Left
 }
@@ -16,23 +16,26 @@ impl Direction {
     }
 }
 
-struct Guard<'a> {
-    row: isize,
-    col: isize,
-    obstacles: &'a HashSet<(isize, isize)>,
-    facing: Direction
+type Row = isize;
+type Col = isize;
+struct Guard {
+    row: Row,
+    col: Col,
+    facing: Direction,
+    starting_row: Row,
+    starting_col: Col,
 }
 
-impl<'a> Guard<'a> {
-    fn new(row: isize, col: isize, obstacles: &'a HashSet<(isize, isize)>) -> Guard<'a> {
-        Guard { row, col, obstacles, facing: Direction::Up }
+impl Guard {
+    fn new(row: isize, col: isize) -> Guard {
+        Guard { row, col, facing: Direction::Up, starting_row: row, starting_col: col }
     }
 
-    fn current_position(&self) -> (isize, isize) {
+    fn current_position(&self) -> (Row, Col) {
         (self.row, self.col)
     }
 
-    fn ahead(&self) -> (isize, isize) {
+    fn ahead(&self) -> (Row, Col) {
         match self.facing {
             Direction::Up => (self.row - 1, self.col),
             Direction::Right => (self.row, self.col + 1),
@@ -41,19 +44,28 @@ impl<'a> Guard<'a> {
         }
     }
 
-    fn step(&mut self) {
+    // Try to make one step forward.  If there is an obstacle there,
+    // then turn instead.
+    fn step(&mut self, obstacles: &HashSet<(Row, Col)>) {
         let ahead = self.ahead();
-        if self.obstacles.contains(&ahead) {
+        if obstacles.contains(&ahead) {
             self.facing.turn();
         } else {
             self.row = ahead.0;
             self.col = ahead.1;
         }
     }
+
+    // Reset the guard to it's starting position and direction
+    fn reset(&mut self) {
+        self.row = self.starting_row;
+        self.col = self.starting_col;
+        self.facing = Direction::Up;
+    }
 }
 
 pub fn part1(input: &str) -> usize {
-    let mut obstacles = HashSet::<(isize, isize)>::new();
+    let mut obstacles = HashSet::<(Row, Col)>::new();
     let mut guard = (0, 0);
     let num_rows = input.lines().count() as isize;
     let num_cols = input.lines().next().unwrap().len() as isize;
@@ -61,25 +73,92 @@ pub fn part1(input: &str) -> usize {
         for (col, ch) in line.chars().enumerate() {
             match ch {
                 '.' => {}
-                '#' => { obstacles.insert((row as isize, col as isize)); }
-                '^' => { guard = (row as isize, col as isize); }
+                '#' => { obstacles.insert((row as Row, col as Col)); }
+                '^' => { guard = (row as Row, col as Col); }
                 other => panic!("Bad input: {}", other)
             }
         }
     }
-    let mut guard = Guard::new(guard.0, guard.1, &obstacles);
+    let mut guard = Guard::new(guard.0, guard.1);
 
-    let mut visited = HashSet::<(isize, isize)>::new();
+    let mut visited = HashSet::<(Row, Col)>::new();
     while (0..num_rows).contains(&guard.row) && (0..num_cols).contains(&guard.col) {
         visited.insert(guard.current_position());
-        guard.step();
+        guard.step(&obstacles);
     }
 
     visited.len()
 }
 
-pub fn part2(_input: &str) -> u32 {
-    43
+//
+// Count how many locations for a single new obstacle that would cause
+// the guard to get into a loop.
+//
+// The brute force solution is to try every possible location that is
+// not an obstacle, and not the guard's initial position, and then
+// see whether the guard exits the grid, or returns to a prior location
+// and direction.
+//
+// We can do at least a little better.  The new obstacle needs to cause
+// the guard to turn towards another obstacle.  It has to be one of the
+// locations the locations they visited in part 1 (i.e. on their path,
+// and in their way, causing them to make a new turn).  It has to be
+// one row/column away from a another obstacle.
+//
+// For example, if the guard was moving left, the new obstacle needs
+// to be one column to the left of an obstacle above the guard's path
+// (since the obstacle will cause them to turn to the right, from Left
+// to Up).  So, if we keep track of both the position and direction they
+// were facing before moving into that position, we only need to search
+// in one direction for a pre-existing obstacle.  I think we still need
+// to simulate their path with the new obstacle (or just the new path
+// after the new turn?) to make sure they are in fact in a loop (and
+// won't exit the grid in a new location).
+//
+pub fn part2(input: &str) -> u32 {
+    let mut obstacles = HashSet::<(Row, Col)>::new();
+    let mut guard = (0, 0);
+    let num_rows = input.lines().count() as isize;
+    let num_cols = input.lines().next().unwrap().len() as isize;
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            match ch {
+                '.' => {}
+                '#' => { obstacles.insert((row as Row, col as Col)); }
+                '^' => { guard = (row as Row, col as Col); }
+                other => panic!("Bad input: {}", other)
+            }
+        }
+    }
+    let mut guard = Guard::new(guard.0, guard.1);
+
+    let mut visited = HashSet::<(Row, Col)>::new();
+    while (0..num_rows).contains(&guard.row) && (0..num_cols).contains(&guard.col) {
+        visited.insert(guard.current_position());
+        guard.step(&obstacles);
+    }
+
+    // Try putting an obstacle at each of the visited positions, reset
+    // the guard to that position, and see whether they get into a loop.
+    visited.remove(&(guard.starting_row, guard.starting_col));
+    let mut result = 0;
+    for (row, col) in visited.iter() {
+        obstacles.insert((*row, *col));
+        let mut visited = HashSet::<(Direction, (Row, Col))>::new();
+        guard.reset();
+        while (0..num_rows).contains(&guard.row) && (0..num_cols).contains(&guard.col) {
+            if !visited.insert((guard.facing, guard.current_position())) {
+                // Found a loop
+                // dbg!((row, col));
+                result += 1;
+                break;
+            }
+            guard.step(&obstacles);
+        }
+        obstacles.remove(&(*row, *col));
+    }
+
+    result
 }
 
 #[test]
@@ -101,6 +180,29 @@ fn test_part1() {
 
 #[test]
 fn test_part2() {
-    let input = "Hello, World!";
-    assert_eq!(part2(input), 43);
+    let input = "\
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+";
+    assert_eq!(part2(input), 6);
+}
+
+const INPUT: &str = include_str!("../input.txt");
+
+#[test]
+fn test_part1_full() {
+    assert_eq!(part1(INPUT), 5461);
+}
+
+#[test]
+fn test_part2_full() {
+    assert_eq!(part2(INPUT), 1836);
 }
