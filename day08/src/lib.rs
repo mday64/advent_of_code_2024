05e0abb477afffec02103e1f7fb2345da2b04b0e
Mark Day <mday@mac.com>
2025-01-18 19:22:00 -0700

    Day 8, part 1

---
+++ day08/src/lib.rs
@@ -0,0 +1,104 @@
+use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
+
+struct Position(i32, i32);
+
+
+pub fn part1(input: &str) -> usize {
+    let num_rows = input.lines().count() as i32;
+    let num_cols = input.lines().next().unwrap().len() as i32;
+    let mut antennas = HashMap::<char, Vec<Position>>::new();
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch != '.' {
+                antennas.entry(ch).or_default().push(Position(row as i32, col as i32));
+            }
+        }
+    }
+
+    let mut results = HashSet::new();
+    
+    for (_ch, positions) in antennas.iter() {
+        for pair in positions.iter().combinations(2) {
+            let d_row = pair[1].0 - pair[0].0;
+            let d_col = pair[1].1 - pair[0].1;
+
+            let t_row = pair[1].0 + d_row;
+            let t_col = pair[1].1 + d_col;
+            if t_row >= 0 && t_row < num_rows && t_col >= 0 && t_col < num_cols {
+                // dbg!((t_row, t_col));
+                results.insert((t_row, t_col));
+            }
+
+            let t_row = pair[0].0 - d_row;
+            let t_col = pair[0].1 - d_col;
+            if t_row >= 0 && t_row < num_rows && t_col >= 0 && t_col < num_cols {
+                // dbg!((t_row, t_col));
+                results.insert((t_row, t_col));
+            }
+        }
+    }
+
+    results.len()
+}
+
+pub fn part2(_input: &str) -> u32 {
+    43
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1_2() {
+        let input = "\
+..........
+..........
+..........
+....a.....
+..........
+.....a....
+..........
+..........
+..........
+..........
+";
+        assert_eq!(part1(input), 2);
+    }
+    
+    #[test]
+    fn test_part1_4() {
+        let input = "\
+..........
+..........
+..........
+....a.....
+........a.
+.....a....
+..........
+..........
+..........
+..........
+";
+        assert_eq!(part1(input), 4);
+    }
+    
+    #[test]
+    fn test_part1_14() {
+        let input = "\
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+        assert_eq!(part1(input), 14);
+    }
+}
