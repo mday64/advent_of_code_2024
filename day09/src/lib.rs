2264418e89261abd79357db216c52dc9c67c5421
Mark Day <mday@mac.com>
2025-01-20 17:46:42 -0700

    Day 9, part 2

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -58,8 +58,74 @@ pub fn part1(input: &str) -> u64 {
     }).sum()
 }
 
-pub fn part2(_input: &str) -> u64 {
-    todo!()
+//
+// I think part 2 would be better served by a different data representation.
+// Instead of keeping track of individual blocks, keep track of runs of
+// blocks belonging to a file or free space.
+//
+pub fn part2(input: &str) -> u64 {
+    type Position = u32;
+    type FileID = u16;
+    type Length = u8;
+    struct FileInfo {
+        position: Position,
+        file_id: FileID,
+        length: Length
+    }
+    struct FreeSpaceInfo {
+        position: Position,
+        length: Length
+    }
+    let mut file_blocks = Vec::<FileInfo>::new();
+    let mut free_blocks = Vec::<FreeSpaceInfo>::new();
+
+    // Parse the input
+    let input = input.trim_end();
+    let input = input.as_bytes();
+    let mut position = 0;
+    let mut file_id = 0;
+    let mut is_free = false;        // Is the current run free space?
+    for byte in input {
+        if !byte.is_ascii_digit() { break; }
+        let length = byte - b'0';
+        if is_free {
+            free_blocks.push(FreeSpaceInfo{position, length});
+        } else {
+            file_blocks.push(FileInfo{position, file_id, length});
+            file_id += 1;
+        }
+        position += length as Position;
+        is_free = !is_free;
+    }
+
+    // Move whole files to the chunk of free space closest to the start
+    // that is big enough to contain the file.
+    for file in file_blocks.iter_mut().rev() {
+        // Try to find some space to move `file`
+        for free in free_blocks.iter_mut() {
+            if free.position > file.position {
+                // No suitable space.  Can't move this file.
+                break;
+            }
+            if free.length < file.length {
+                // Look for a longer free run
+                continue;
+            }
+
+            // Move the file
+            file.position = free.position;
+            free.position += file.length as u32;
+            free.length -= file.length;
+            break;
+        }
+    }
+
+    // Calculate the checksum
+    file_blocks.into_iter().map(|file| {
+        ((file.position as u64) .. (file.position as u64 + file.length as u64))
+        .sum::<u64>() * file.file_id as u64
+    })
+    .sum()
 }
 
 #[cfg(test)]
@@ -80,4 +146,9 @@ fn test_part1_full() {
 fn test_part2() {
     let input = "2333133121414131402";
     assert_eq!(part2(input), 2858);
+}
+
+#[test]
+fn test_part2_full() {
+    assert_eq!(part2(FULL_INPUT), 6390781891880);
 }
