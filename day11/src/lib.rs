25c8a058387bf0e7fdb2eff10e1e3f13a4899738
Mark Day <mday@mac.com>
2025-02-12 14:49:12 -0700

    Day 11, part 1 faster (same algo as part 2)

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -1,38 +1,13 @@
 use std::collections::HashMap;
 
 pub fn part1(input: &str) -> usize {
-    let mut numbers: Vec<u64> = input
+    let mut cache: HashMap<(u64, usize), usize> = HashMap::new();
+    let seeds = input
         .trim_end()
         .split_ascii_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
+        .map(|s| s.parse().unwrap());
 
-    for _ in 0..25 {
-        numbers = part1_iteration(&numbers);
-    }
-
-    numbers.len()
-}
-
-fn part1_iteration(numbers: &[u64]) -> Vec<u64> {
-    let mut result = Vec::new();
-    for number in numbers {
-        match number {
-            0 => result.push(1),
-            10..=99 => { result.push(number/10); result.push(number%10); }
-            1000..=9999 => { result.push(number/100); result.push(number%100); }
-            100000..=999999 => { result.push(number/1000); result.push(number%1000); }
-            10000000..=99999999 => { result.push(number/10000); result.push(number%10000); }
-            1000000000..=9999999999 => { result.push(number/100000); result.push(number%100000); }
-            100000000000..=999999999999 => { result.push(number/1000000); result.push(number%1000000); }
-            10000000000000..=99999999999999 => { result.push(number/10000000); result.push(number%10000000); }
-            1000000000000000..=9999999999999999 => { result.push(number/100000000); result.push(number%100000000); }
-            100000000000000000..=999999999999999999 => { result.push(number/1000000000); result.push(number%1000000000); }
-            10000000000000000000.. => { result.push(number/10000000000); result.push(number%10000000000); }
-            _ => result.push(number * 2024)
-        }
-    }
-    result
+    seeds.map(|seed| expanded_length(seed, 25, &mut cache)).sum()
 }
 
 fn iterate_one_number(number: u64) -> (u64, Option<u64>) {
