5f32119638aa46f1935f8d77fdce53313b2014dc
Mark Day <mday@mac.com>
2025-02-12 19:19:53 -0700

    Day 11: reformat

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -7,27 +7,33 @@ pub fn part1(input: &str) -> usize {
         .split_ascii_whitespace()
         .map(|s| s.parse().unwrap());
 
-    seeds.map(|seed| expanded_length(seed, 25, &mut cache)).sum()
+    seeds
+        .map(|seed| expanded_length(seed, 25, &mut cache))
+        .sum()
 }
 
 fn iterate_one_number(number: u64) -> (u64, Option<u64>) {
     match number {
         0 => (1, None),
-        10..=99 => (number/10, Some(number%10)),
-        1000..=9999 => (number/100, Some(number%100)),
-        100000..=999999 => (number/1000, Some(number%1000)),
-        10000000..=99999999 => (number/10000, Some(number%10000)),
-        1000000000..=9999999999 => (number/100000, Some(number%100000)),
-        100000000000..=999999999999 => (number/1000000, Some(number%1000000)),
-        10000000000000..=99999999999999 => (number/10000000, Some(number%10000000)),
-        1000000000000000..=9999999999999999 => (number/100000000, Some(number%100000000)),
-        100000000000000000..=999999999999999999 => (number/1000000000, Some(number%1000000000)),
-        10000000000000000000.. => (number/10000000000, Some(number%10000000000)),
-        _ => (number * 2024, None)
+        10..=99 => (number / 10, Some(number % 10)),
+        1000..=9999 => (number / 100, Some(number % 100)),
+        100000..=999999 => (number / 1000, Some(number % 1000)),
+        10000000..=99999999 => (number / 10000, Some(number % 10000)),
+        1000000000..=9999999999 => (number / 100000, Some(number % 100000)),
+        100000000000..=999999999999 => (number / 1000000, Some(number % 1000000)),
+        10000000000000..=99999999999999 => (number / 10000000, Some(number % 10000000)),
+        1000000000000000..=9999999999999999 => (number / 100000000, Some(number % 100000000)),
+        100000000000000000..=999999999999999999 => (number / 1000000000, Some(number % 1000000000)),
+        10000000000000000000.. => (number / 10000000000, Some(number % 10000000000)),
+        _ => (number * 2024, None),
     }
 }
 
-fn expanded_length(number: u64, iterations: usize, cache: &mut HashMap<(u64, usize), usize>) -> usize {
+fn expanded_length(
+    number: u64,
+    iterations: usize,
+    cache: &mut HashMap<(u64, usize), usize>,
+) -> usize {
     if iterations == 0 {
         return 1;
     }
@@ -36,8 +42,11 @@ fn expanded_length(number: u64, iterations: usize, cache: &mut HashMap<(u64, usi
     }
 
     let result = match iterate_one_number(number) {
-        (num1, None) => expanded_length(num1, iterations-1, cache),
-        (num1, Some(num2)) => expanded_length(num1, iterations-1, cache) + expanded_length(num2, iterations-1, cache),
+        (num1, None) => expanded_length(num1, iterations - 1, cache),
+        (num1, Some(num2)) => {
+            expanded_length(num1, iterations - 1, cache)
+                + expanded_length(num2, iterations - 1, cache)
+        }
     };
     cache.insert((number, iterations), result);
     result
@@ -50,7 +59,9 @@ pub fn part2(input: &str) -> usize {
         .split_ascii_whitespace()
         .map(|s| s.parse().unwrap());
 
-    seeds.map(|seed| expanded_length(seed, 75, &mut cache)).sum()
+    seeds
+        .map(|seed| expanded_length(seed, 75, &mut cache))
+        .sum()
 }
 
 #[test]
