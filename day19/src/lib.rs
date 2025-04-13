e65ac1f2ad5643eac436fc1b0cffc0158c1c5025
Mark Day <mday@mac.com>
2025-04-13 14:30:59 -0600

    Day 19, part 2

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use nom::{bytes::tag, character::complete::alpha1, multi::separated_list1, sequence::separated_pair, Parser, IResult};
 
 fn parse_input(input: &str) -> IResult<&str, (Vec<&str>, Vec<&str>)> {
@@ -20,20 +22,31 @@ pub fn part1(input: &str) -> usize {
     patterns.iter().filter(|p| pattern_from_towels(p, &towels)).count()
 }
 
+//
+// A naive recursive solution is far too slow.
+//
+// Let's try a dynamic programming approach.
+//
 pub fn part2(input: &str) -> usize {
-    fn pattern_from_towels(pattern: &str, towels: &[&str]) -> usize {
-        if pattern.is_empty() { return 1 }
-        towels.iter().map(|t|
-            if let Some(tail) = pattern.strip_prefix(t) {
-                pattern_from_towels(tail, towels)
-            } else {
-                0
-            }
-        ).sum()
-    }
-
     let (_remaining, (towels, patterns)) = parse_input(input).expect("parse");
-    patterns.iter().map(|p| pattern_from_towels(p, &towels)).sum()
+    patterns.iter().map(|p| {
+        let mut cache = HashMap::<&str,usize>::new();
+        for i in (0..p.len()).rev() {
+            let suffix = &p[i..];
+            let mut combos = 0;
+            for towel in &towels {
+                if let Some(tail) = suffix.strip_prefix(towel) {
+                    combos += if tail.is_empty() {
+                        1
+                    } else {
+                        *cache.get(&tail).unwrap_or(&0)
+                    }
+                }
+            }
+            cache.insert(suffix, combos);
+        }
+        *cache.get(p).unwrap()
+    }).sum()
 }
 
 #[test]
@@ -70,10 +83,33 @@ bbrgwb
     assert_eq!(part2(input), 16);
 }
 
+#[test]
+fn test_part2_overlapping() {
+    let input = "\
+r, wr, b, g, bwu, rb, gb, br
+
+gbr
+";
+    // The possibilities are:
+    //      g, b, r
+    //      gb, r
+    //      g, br
+    // Note that we can't break it down into either
+    //      (g) * (br)
+    //      (gb) * r
+    // both of which would return 2
+    assert_eq!(part2(input), 3);
+}
+
 #[cfg(test)]
 const FULL_INPUT: &str = include_str!("../input.txt");
 
 #[test]
 fn test_part1_full() {
     assert_eq!(part1(FULL_INPUT), 238);
+}
+
+#[test]
+fn test_part2_full() {
+    assert_eq!(part2(FULL_INPUT), 635018909726691);
 }
