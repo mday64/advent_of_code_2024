b781452d14601183abae9579c81d2eefd07952b2
Mark Day <mday@mac.com>
2025-04-25 10:10:24 -0600

    Day 25

--- day25/src/lib.rs
+++ day25/src/lib.rs
@@ -1,5 +1,38 @@
-pub fn part1(_input: &str) -> String {
-    "Hello".to_string()
+//
+// The goal is to count how many unique pairs of lock and key do not
+// overlap.
+//
+// The problem talks about the "height" or length in each column, but
+// that's not necessary to solve part 1.  All we need to do is check
+// for overlapping '#' characters.  We don't even need to split objects
+// into key vs. lock; keys will always overlap with keys, and locks
+// will always overlap with locks.
+//
+// Each key/lock is going to be represented by an integer (turning the
+// characters of the input into a bitmap).  The test for overlap is
+// bitwise AND.
+//
+// We will take advantage of the fact that b'#' is odd, while b'.'
+// and b'\n' are even.  We'll build a bitmap of the input characters,
+// using the least significant bit of each character.  Note that each
+// key/lock occupies 43 bytes in the input (including the blank line
+// at the end of each one).
+//
+pub fn part1(input: &str) -> u32 {
+    let items:Vec<u64> = input.as_bytes().chunks(43).map(|chunk| {
+        chunk[0..42].iter().fold(0u64, |v, byte| v * 2 + (byte & 1) as u64)
+    }).collect();
+
+    let mut result = 0;
+    for i in 0..items.len()-1 {
+        for j in (i+1)..items.len() {
+            if items[i] & items[j] == 0 {
+                result += 1;
+            }
+        }
+    }
+
+    result
 }
 
 pub fn part2(_input: &str) -> String {
@@ -8,12 +41,60 @@ pub fn part2(_input: &str) -> String {
 
 #[test]
 fn test_part1() {
-    let input = "Hello, World!";
-    assert_eq!(part1(input), "Hello");
+    let input = "\
+#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####
+";
+    assert_eq!(part1(input), 3);
 }
 
 #[test]
 fn test_part2() {
     let input = "Hello, World!";
     assert_eq!(part2(input), "World");
+}
+
+#[cfg(test)]
+static FULL_INPUT: &str = include_str!("../input.txt");
+
+#[test]
+fn test_part1_full() {
+    assert_eq!(part1(FULL_INPUT), 3107);
 }
